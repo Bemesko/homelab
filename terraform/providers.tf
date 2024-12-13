9c76d8c309465697c0d266b6fcc9bfd295adb98a
Bernardo Pires Mesko <bemesko@gmail.com>
2024-12-13 06:52:27 -0300

    Initial terraform configuration

---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "73a487bd-8891-4ac0-a7aa-c34fb77197c7"
+}
