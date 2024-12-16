3eb4f39d277a696803973132b34a650a85e7911f
Bernardo Pires Mesko <bemesko@gmail.com>
2024-12-16 14:14:51 -0300

    Migrate config to AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,62 +1,38 @@
-resource "azurerm_resource_group" "bernetes" {
-  name     = "rg-bernetes"
-  location = var.location
-}
+resource "aws_instance" "bernetes" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t3.micro"
 
-resource "azurerm_linux_virtual_machine" "bernetes" {
-  name                  = "vm-bernetes-01"
-  resource_group_name   = azurerm_resource_group.bernetes.name
-  location              = azurerm_resource_group.bernetes.location
-  size                  = "Standard_B2ps_v2" # TODO: Find a free size that supports x64
-  admin_username        = var.admin_username
-  network_interface_ids = [azurerm_network_interface.bernetes.id]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+  tags = {
+    Name = "Free-Tier-VM"
   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = 30
+  # Optional: Add a security group to allow SSH
+  vpc_security_group_ids = [aws_security_group.bernetes.id]
+
+  # Optional: Add a key pair for SSH access
+  key_name = aws_key_pair.bernetes.key_name
+}
+
+resource "aws_security_group" "bernetes" {
+  name_prefix = "allow-ssh-"
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_virtual_network" "bernetes" {
-  name                = "vnet-bernetes-01"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.bernetes.location
-  resource_group_name = azurerm_resource_group.bernetes.name
-}
-
-resource "azurerm_subnet" "bernetes" {
-  name                 = "subnet-bernetes-01"
-  resource_group_name  = azurerm_resource_group.bernetes.name
-  virtual_network_name = azurerm_virtual_network.bernetes.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "bernetes" {
-  name                = "nic-bernetes-01"
-  location            = azurerm_resource_group.bernetes.location
-  resource_group_name = azurerm_resource_group.bernetes.name
-
-  ip_configuration {
-    name                          = "ipconfig-bernetes-01"
-    subnet_id                     = azurerm_subnet.bernetes.id
-    private_ip_address_allocation = "Dynamic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "vm_id" {
-  description = "ID of the provisioned virtual machine."
-  value       = azurerm_linux_virtual_machine.bernetes.id
+resource "aws_key_pair" "bernetes" {
+  key_name   = "free-tier-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Update with your actual public key path
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,21 +2,12 @@ terraform {
   required_version = ">=1.9.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.1.0"
-    }
 
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.azure_subscription_id
 }
 
 provider "aws" {
