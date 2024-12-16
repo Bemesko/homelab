ec3716ff9c7f7a2e3d1e63e4f33abe78d360615c
Bernardo Pires Mesko <bemesko@gmail.com>
2024-12-16 13:18:04 -0300

    Add AWS terraform provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = ">=1.9.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~>4.1.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -10,4 +17,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = var.azure_subscription_id
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
