ead9b3c9494d2a0fafa0be07c9e1a2c041d912fd
Bernardo Pires Mesko <bemesko@gmail.com>
2024-12-20 18:44:52 -0300

    Associate public IP with EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,46 @@
+# user here is ec2-user
 resource "aws_instance" "bernetes" {
   ami               = "ami-0453ec754f44f9a4a"
   instance_type     = "t3.micro"
+  subnet_id         = aws_subnet.bernetes.id
   availability_zone = "us-east-1a"
+
+  vpc_security_group_ids = [aws_security_group.bernetes.id]
 
   tags = {
     Name = "Free-Tier-VM"
   }
 
   key_name = aws_key_pair.bernetes.key_name
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.bernetes.id
-  }
-}
-
-resource "aws_network_interface" "bernetes" {
-  subnet_id  = aws_subnet.bernetes.id
-  private_ip = "10.0.0.10"
-  security_groups = [
-    aws_security_group.bernetes.id
-  ]
 }
 
 resource "aws_vpc" "bernetes" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "bernetes" {
+  vpc_id = aws_vpc.bernetes.id
+}
+
+resource "aws_route_table" "bernetes" {
+  vpc_id = aws_vpc.bernetes.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.bernetes.id
+  }
+}
+
+resource "aws_route_table_association" "bernetes" {
+  subnet_id      = aws_subnet.bernetes.id
+  route_table_id = aws_route_table.bernetes.id
+}
+
 resource "aws_subnet" "bernetes" {
-  vpc_id            = aws_vpc.bernetes.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.bernetes.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "bernetes" {
